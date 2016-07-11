cdb2f4cdaa9db0c716d3ea15ce8e1a8cc08f04b5
Dave <dave@d53dev.net>
2016-07-11 16:46:18 +0200

    Added empty xcode project

---
+++ WeatherInFreakingMenuBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WeatherInFreakingMenuBar
+//
+//  Created by David Sere on 11/07/16.
+//  Copyright © 2016 d53dev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
